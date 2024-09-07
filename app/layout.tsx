c595352ca2459b73fd002b7f3a545710e22ec30d
Jasper Ngunjiri Wambugu <jasperwambugu@gmail.com>
2024-09-08 00:10:25 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { ThemeProvider } from "./Provider";
+
+export const metadata: Metadata = {
+  title: "Jasper Wambugu Portfolio",
+  description: "Modern and Sleek Js Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
